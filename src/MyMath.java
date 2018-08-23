793696ea5d4d8a1b0eee873dcfe83af0c03790ad
Владислав <vladislav.xiv@gmail.com>
2018-08-23 16:15:29 +0300

    unused

---
+++ src/MyMath.java
@@ -0,0 +1,14 @@
+public class MyMath {
+
+    public static void main(String[] args){
+    int a=999;
+    int b=888;
+
+    System.out.println(a+b);
+    System.out.println(b-a);
+    System.out.println(b/a);
+    System.out.println(b*b);
+    System.out.println((a+b)%a);
+
+    }
+}
